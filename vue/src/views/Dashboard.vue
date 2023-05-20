d43d37df0d32aa23e6bcf66c83464a4162aa1e46
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2023-05-20 21:05:20 +0300

    Create reusable PageComponent

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,23 +1,13 @@
 <template>
-  <div>
-    <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-      </div>
-    </header>
-    <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
-      </div>
-    </main>
-  </div>
-
+  <pageComponent title="Dashboard">
+    Content goes here
+  </pageComponent>
 </template>
 
-<script>
-export default {
-  name: "Dashboard"
-}
+<script setup>
+
+import pageComponent from '../components/pageComponent.vue';
+
 </script>
 
 <style scoped>
--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,19 +1,15 @@
 <template>
-  <div>
-    <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
-      </div>
-    </header>
-    <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
-        Content Goes here
-      </div>
-    </main>
-  </div>
+  <pageComponent title="Surveys">
+    Content goes here
+  </pageComponent>
 </template>
 
 <script setup>
 
+import pageComponent from '../components/pageComponent.vue';
+
 </script>
+
+<style scoped>
+
+</style>
